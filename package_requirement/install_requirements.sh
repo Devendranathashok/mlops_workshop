5219c1312621ae98265c64d9a9c2fdc54c35ce5b
Devendranathashok <ashok.kammala@gmail.com>
2024-09-14 15:48:43 +0530

    Update install_requirements.sh

--- package_requirement/install_requirements.sh
+++ package_requirement/install_requirements.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python --version
-pip install --upgrade azure-cli
-pip install --upgrade azureml-sdk
+pip3 install --upgrade azure-cli
+pip3 install --upgrade azureml-sdk
 PIP_NO_BUILD_ISOLATION=1 
-pip install -r requirements_2.txt
+pip3 install -r requirements_2.txt
